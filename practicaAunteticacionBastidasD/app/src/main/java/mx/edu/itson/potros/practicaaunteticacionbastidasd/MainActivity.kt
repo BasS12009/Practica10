a9d894e9e837672bb158d6044b00cb7bd347081d
BasS12009 <149912863+BasS12009@users.noreply.github.com>
2025-03-25 18:29:44 -0700

    dd

---
+++ practicaAunteticacionBastidasD/app/src/main/java/mx/edu/itson/potros/practicaaunteticacionbastidasd/MainActivity.kt
@@ -0,0 +1,38 @@
+package mx.edu.itson.potros.practicaaunteticacionbastidasd
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val email = intent.extras!!.getString("user")
+
+        val user: TextView = findViewById(R.id.tvUser)
+        val button: Button = findViewById(R.id.btnLogout)
+
+        user.text = "Bienvenido, " + email
+
+
+        button.setOnClickListener({
+            FirebaseAuth.getInstance().signOut()
+            val intent = Intent(this, login::class.java)
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+        })
+
+    }}
+
+
